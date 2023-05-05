8aaa1dc7dea601890bbede36eb14126b616b0378
Raja-Junaid <rajajunaidpdk@gmail.com>
2023-05-05 19:18:08 +0500

    Remove overlay

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,17 @@
-const openMenu = document.querySelector('.nav-hamburger');
-const closeMenu = document.querySelector('.nav-close');
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
+const openMenu = document.querySelector(".nav-hamburger");
+const closeMenu = document.querySelector(".nav-close");
+const modal = document.querySelector(".modal");
 
-openMenu.addEventListener('click', function () {
-  modal.classList.remove('hidden');
-  modal.classList.add('flex');
-  openMenu.classList.add("hidden")
-  closeMenu.classList.remove('hidden');
+openMenu.addEventListener("click", function () {
+  modal.classList.remove("hidden");
+  modal.classList.add("flex");
+  openMenu.classList.add("hidden");
+  closeMenu.classList.remove("hidden");
 });
 
-closeMenu.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  modal.classList.remove('flex');
-  openMenu.classList.remove("hidden")
-  closeMenu.classList.add('hidden');
+closeMenu.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  modal.classList.remove("flex");
+  openMenu.classList.remove("hidden");
+  closeMenu.classList.add("hidden");
 });
