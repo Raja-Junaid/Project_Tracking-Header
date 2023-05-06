63ed897f849f81fbd683943d977983496c629c34
Raja-Junaid <rajajunaidpdk@gmail.com>
2023-05-06 11:43:21 +0500

    Refactor code in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         veryDarkBlue: "hsl(230, 29%, 20%)",
         darkGrayishBlue: "hsl(230, 11%, 40%)",
         grayishBlue: "hsl(231, 7%, 65%)",
-        lightGrayishBlue: "hsl(207, 33%, 95%)"
+        lightGrayishBlue: "hsl(207, 33%, 95%)",
       },
     },
   },
